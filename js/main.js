9b16c80382a538c84a735b6092504b081d3d0a7c
Janos Tordai <tjanos88@gmail.com>
2023-02-18 02:00:32 +0100

    Added almost all functionality

--- js/main.js
+++ js/main.js
@@ -1,36 +1,94 @@
-function drawCanvas(size) {
+const BASE_SIZE = 16;
+const BASE_COLOR = "#000000";
+const BASE_MODE = "color";
+const CANVAS_SIZE = 500;
+
+let currentSize = BASE_SIZE;
+let currentColor = BASE_COLOR;
+let currentMode = BASE_MODE;
+
+
+const colorPicker = document.querySelector('#colorPicker');
+const colorButton = document.querySelector("#colorButton");
+const eraseButton = document.querySelector("#eraseButton");
+const clearButton = document.querySelector("#clearButton");
+const sliderText = document.querySelector("#sliderValue");
+const slider = document.querySelector("#slider");
+const canvas = document.querySelector('#canvas');
+
+
+slider.onchange = (e) => changeSize(e.target.value);
+colorButton.onclick = (e) => updateCurrentMode(e.target);
+eraseButton.onclick = (e) => updateCurrentMode(e.target);
+clearButton.onclick = () => clearGrid();
+
+
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
+
+function changeSize(size) {
+    updateCurrentSize(size)
+    updateSizeText(size);
+    drawGrid(size);
+}
+
+function updateCurrentSize(size) {
+    currentSize = size;
+}
+
+function updateSizeText(size) {
+    sliderText.innerHTML = size + 'x' + size;
+}
+
+function updateCurrentMode(target) {
+    removeActiveButtonClass();
+    activateButton(target);
+    currentMode = target.value;
+}
+
+function activateButton(button) {
+    button.classList.add("activeButton");
+}
+
+function removeActiveButtonClass() {
+    document.querySelectorAll("button").forEach(button => {
+        button.classList.remove("activeButton")
+    })
+}
+
+function drawGrid(size) {
+    canvas.innerHTML = "";
     for (let i = 0; i < size; i++) {
         for (let j = 0; j < size; j++) {
-            console.log(i, j);
-            let cell = document.createElement('div');
-            cell.className = 'cell';
-            cell.style['width'] = '25px';
-            cell.style['height'] = '25px';
-            document.querySelector('.canvas').appendChild(cell);
+            let cell = document.createElement("div");
+            cell.className = "cell";
+            cell.style.width = CANVAS_SIZE / size + "px";
+            cell.style.height = CANVAS_SIZE / size + "px";
+            cell.addEventListener("mouseover", colorizeCell);
+            cell.addEventListener("mousedown", colorizeCell);
+            canvas.appendChild(cell);
         }
     }
 }
 
-function updateCanvasSize(size) {
-    let canvas = document.querySelector('.canvas');
-    let value = (size + 1) * 25 + 1 + 'px'
-    canvas.style.width = value;
-    canvas.style.height = value;
+function colorizeCell(event){
+    if (event.type === "mouseover" && !mouseDown) return
+    if (currentMode === "color") {
+        event.target.style.backgroundColor = colorPicker.value;
+    }
+    else if (currentMode === "erase"){
+        event.target.style.backgroundColor = "#ffffff";
+    }
 }
 
+function clearGrid(){
+    document.querySelectorAll('.cell').forEach(cell => {
+        cell.style.backgroundColor = "#ffffff";
+    })
+}
 
 window.onload = () => {
-    let size = parseInt(document.querySelector("#canvas-size").value);
-    console.log("Setting default canvas")
-    console.log(size);
-    drawCanvas(size);
-
-
-    document.querySelector("#canvas-size").addEventListener("change", (event) => {
-        let size = parseInt(event.target.value);
-        console.log("Changing canvas")
-        console.log(size);
-        drawCanvas(size);
-        updateCanvasSize(size);
-    })
+    drawGrid(BASE_SIZE);
+    activateButton(colorButton);
 }
